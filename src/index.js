529d4fd2e768390b3791646aaac6b5617926869f
Iana Kozma <yana.kozma@gmail.com>
2023-03-01 16:04:38 +0100

    homework 10 js

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { countryСardTemplate, countryListTemplate } from './js/markupTemplate';
+import { refs } from './js/refs-elements';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 300;
+
+refs.searchForm.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
+
+function onInputCountry() {
+    const countryName = refs.searchForm.value;
+    if (countryName === '') {
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+    }
+
+    fetchCountries(countryName)
+        .then(countries => {
+            if (countries.length > 10) {
+                Notify.info('Too many matches found. Please enter a more specific name.');
+                refs.countryInfo.innerHTML = '';
+                refs.countryList.innerHTML = '';
+                return;
+            }
+
+            if (countries.length <= 10) {
+                const listMarkup = countries.map(country => countryListTemplate(country));
+                refs.countryList.innerHTML = listMarkup.join('');
+                refs.countryInfo.innerHTML = '';
+            }
+
+            if (countries.length === 1) {
+                const markup = countries.map(country => countryСardTemplate(country));
+                refs.countryInfo.innerHTML = markup.join('');
+                refs.countryList.innerHTML = '';
+            }
+        })
+        .catch(error => {
+            Notify.failure('Oops, there is no country with that name');
+            refs.countryInfo.innerHTML = '';
+            refs.countryList.innerHTML = '';
+            return error;
+        });
+}
---
+++ src/js/refs-elements.js
@@ -0,0 +1,5 @@
+export const refs = {
+  searchForm: document.querySelector("#search-box"),
+  countryList: document.querySelector('ul.country-list'),
+  countryInfo: document.querySelector('div.country-info'),
+};
